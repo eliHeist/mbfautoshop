016261a7b8f816acf7a9fae603749a011d6a1ef7
Elijah Triumph <eliyang256@outlook.com>
2025-04-16 10:47:28 +0300

    fix static root

--- mbf_auto_shop/settings.py
+++ mbf_auto_shop/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mbf_auto_shop/assets/static/dist/'),
     os.path.join(BASE_DIR, 'mbf_auto_shop/assets/assets/'),
 ]
-STATIC_ROOT = BASE_DIR, 'mbf_auto_shop/assets/staticfiles'
+STATIC_ROOT = BASE_DIR / 'mbf_auto_shop/assets/staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mbf_auto_shop/assets/media')
